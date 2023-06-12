77b2bcc59c032fae77db8310c55b667c0a1642ce
markjt031 <markjt031@gmail.com>
2023-06-12 12:43:48 -0400

    changed styles for very large screens

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
         <div className={styles.container}>
             <Image
               src="/The-Legend-of-Zelda-Tears-of-the-Kingdom-Walkthrough-and-Strategy-Guide-1024x576.jpg"
@@ -14,10 +14,10 @@ export default function Home() {
               priority
             />
         </div>
-        <div>
+        <div className={styles.text}>
           <h1>Welcome to Hyrule Archives!</h1>
           <p>Your one stop for Legend of Zelda: Tears of the Kingdom information. Create an account to add your own compendium information, shrine guides, and Korok sightings!</p>
         </div>
-    </main>
+    </>
   )
 }
