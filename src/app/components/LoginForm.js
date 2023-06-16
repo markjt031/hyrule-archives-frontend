5f91ec02a06e8559eaa5828477c7fa86e1b87f8b
markjt031 <markjt031@gmail.com>
2023-06-16 14:16:56 -0400

    changed confirm delete button

--- src/app/components/LoginForm.js
+++ src/app/components/LoginForm.js
@@ -27,7 +27,7 @@ export default function LoginForm() {
   }
 
   const handleLogin = async (userObj) => {
-    const response= await fetch(`http://localhost:3002/users/login`,
+    const response= await fetch(`https://hyrule-archive.herokuapp.com/users/login`,
     {
         method: "POST",
         mode: "cors",
